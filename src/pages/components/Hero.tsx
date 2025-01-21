1ca7a3e2b324b7d4fa1caf2444e02290b04cf9f7
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-21 23:57:44 +0500

    Update Hero.tsx

--- src/pages/components/Hero.tsx
+++ src/pages/components/Hero.tsx
@@ -6,6 +6,8 @@ interface Product {
   id: string;
   title: string;
   price: number;
+  colors:string;
+  size:string;
   category: string;
   imageUrl: string;
   slug?: { current: string };
@@ -73,20 +75,21 @@ const Hero: React.FC = () => {
       >
         <div className="combine div1 flex">
           {filteredProducts.map((key: Product) => (
+            <Link href={`/product_details/${key.slug?.current || ""}`}>
             <div
               key={key.id}
               style={{ width: "300px", height: "393px", marginLeft: "48px" }}
-              className="bg-gray-100 p-4"
+              className="bg-gray-100 p-4 bottom-4 relative"
             >
               {key.imageUrl && (
-                <Link href={`/product_details/${key.slug?.current || ""}`}>
+                
                 <Image
-                  width={300}
-                  height={300}
+                  width={250}
+                  height={50}
                   src={key.imageUrl}
                   alt="Product Image"
                   className="rounded"
-                /></Link>
+                />
               )}
 
               <div className="mt-4">
@@ -104,10 +107,45 @@ const Hero: React.FC = () => {
                   <h2 className="text-gray-600 text-sm">Mens Short-Sleeve</h2>
                   <h2 className="text-gray-600 text-sm">{key.category}</h2>
                 </div>
+
+                <div className="top-2 right-1 relative">
+              {Array.isArray(key.colors) && key.colors.includes("red") && (
+              <button className="border-2 border-gray-300 ml-1 mx-1 bg-red-700 rounded-full w-6 h-6"></button>
+              )}
+              
+              {Array.isArray(key.colors) && key.colors.includes("green") && (
+              <button className="border-2 border-gray-300 ml-1 mx-1 bg-green-700 rounded-full w-6 h-6"></button>
+              )} 
+              
+              {Array.isArray(key.colors) && key.colors.includes("blue") && (
+              <button className="border-2 border-gray-300 ml-1 mx-1 bg-blue-700 rounded-full w-6 h-6"></button>
+              )}
+              
+              {Array.isArray(key.colors) && key.colors.includes("pink") && (
+              <button className="border-2 border-gray-300 ml-1 mx-1 bg-pink-700 rounded-full w-6 h-6"></button>
+              )}
+              
+              {Array.isArray(key.colors) && key.colors.includes("black") && (
+              <button className="border-2 border-gray-300 ml-1 mx-1 bg-gray-900 rounded-full w-6 h-6"></button>
+              )}
               </div>
+
+            <div className="left-0 top-5 relative">
+            {Array.isArray(key.size) && key.size.includes('S') && <span className="border border-gray-300 px-1 mx-1">S</span>}
+            {Array.isArray(key.size) && key.size.includes('M') && <span className="border border-gray-300 px-1 mx-1">M</span>}
+            {Array.isArray(key.size) && key.size.includes('L') && <span className="border border-gray-300 px-1 mx-1">L</span>}
+            {Array.isArray(key.size) && key.size.includes('XL') && <span className="border border-gray-300 px-1 mx-1">XL</span>}
+            {Array.isArray(key.size) && key.size.includes('XXL') && <span className="border border-gray-300 px-1 mx-1">XXL</span>}
             </div>
+
+
+
+              </div>
+            </div></Link>
           ))}
+          
         </div>
+        
       </div>
     </section>
   );
