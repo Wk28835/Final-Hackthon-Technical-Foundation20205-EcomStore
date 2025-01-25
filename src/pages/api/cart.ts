5f09a34ce10639504a2e27606ca0493ca6c97c13
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-25 21:25:58 +0500

    Update cart.ts

--- src/pages/api/cart.ts
+++ src/pages/api/cart.ts
@@ -1,12 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import sanityClient from "../../sanity/lib/client"
+import sanityClient from "../../sanity/lib/client";
 
-
-export default async function handler( req: NextApiRequest, res: NextApiResponse) 
-{
-
-if(req.method === "GET"){
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
     const query = `*[_type == "cart"]{
         _id,
         title,
@@ -18,29 +15,30 @@ if(req.method === "GET"){
         colors,
         status,
         slug
-        }`;
-  
-  // If category is defined, pass it in the parameters object
-  const cartItems = await sanityClient.fetch(query);
-  
-  res.status(200).json(cartItems);
-}
-if (req.method === "DELETE") {
-  const { cartId } = req.body;
+    }`;
 
-  if (!cartId) {
-    return res.status(400).json({ message: "Product ID is required!" });
+    try {
+      const cartItems = await sanityClient.fetch(query);
+      res.status(200).json(cartItems);
+    } catch {
+      res.status(500).json({ message: "Failed to fetch cart items!" });
+    }
+  } else if (req.method === "DELETE") {
+    const { cartId } = req.body;
+
+    if (!cartId) {
+      return res.status(400).json({ message: "Product ID is required!" });
+    }
+
+    try {
+      await sanityClient.delete(cartId);
+      return res.status(200).json({ message: "Product Deleted Successfully!" });
+    } catch {
+      return res.status(500).json({ message: "Failed to delete product!" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed!" });
   }
+};
 
-  try {
-    // Perform the delete operation in Sanity
-    await sanityClient.delete(cartId);
-
-    return res.status(200).json({ message: "Product Deleted Successfully!" });
-  } catch  {
-    return res.status(500).json({ message: "Failed to delete product!" });
-  }
-}
-
-return res.status(405).json({ error: "Method Not Allowed!" });
-}
+export default handler;
