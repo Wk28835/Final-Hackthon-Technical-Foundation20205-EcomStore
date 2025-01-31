43dc539a6532ba4f3d6e85ad793fef9ff7aef172
Wk28835 <163337656+Wk28835@users.noreply.github.com>
2025-01-31 15:51:36 +0500

    Update create-payment-intent.ts

--- src/pages/api/create-payment-intent.ts
+++ src/pages/api/create-payment-intent.ts
@@ -18,7 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       res.status(200).json({ clientSecret: paymentIntent.client_secret });
       
-    } catch (error) {
+    } catch {
       res.status(500).json({ error: "failed to proceed!" });
     }
   } else {
